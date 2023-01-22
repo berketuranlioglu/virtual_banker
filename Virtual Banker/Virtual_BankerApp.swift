652ab17d15c3377848833c0a892b7b13938e26e5
Berke Turanlıoğlu <berke.turanlioglu@gmail.com>
2023-01-22 20:01:03 +0300

    Initial Commit

---
+++ Virtual Banker/Virtual_BankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_BankerApp.swift
+//  Virtual Banker
+//
+//  Created by Berke Turanlıoğlu on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_BankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
